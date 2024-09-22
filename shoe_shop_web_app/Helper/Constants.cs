89bd7f510d1a51a522e690f5b703716b7e198347
DESKTOP-DVQ6AB1\caiba <caibaotoan@gmail.com>
2024-09-22 20:56:07 +0700

    commit

--- shoe_shop_web_app/Helper/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace shoe_shop_web_app.Helper
-{
-    public class Constants
-    {
-        public static string DOMAIN_URL = "http://localhost:5222";
-        public static string DOMAIN_URL_AZURE = "https://app-shoe-shop-beta.azurewebsites.net";
-    }
-}
